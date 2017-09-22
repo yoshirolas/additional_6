80e6aa6158eb48e19cfac174e898ece6b830a74a
yoshirolas <yoshirolas@tut.by>
2017-09-22 14:13:40 +0300

    zeros/del: unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 module.exports = function zeros(expression) {
       var arrExpression = expression.split('*');
-      //alert(arrExpression);
       var result = 1;
       var zeros = 0;
       var countEven = 0;
       for (var i = 0; i < arrExpression.length; i++) {
         var str = arrExpression[i];
-        //alert(str);
         var factorialDobleIndex = str.indexOf("!!");
 //this code for counting !Factorial
         if (factorialDobleIndex == -1 ) {
           var factorialIndex = str.indexOf("!");
           var number = +arrExpression[i].slice(0, factorialIndex);
-          //alert(number);
           var factorial = 1;
           var count = 0;
           for (var j = 1; j < number; j++) {
@@ -28,7 +25,6 @@ module.exports = function zeros(expression) {
 //this code for counting !!Double Factorial
         else {
           var number = +arrExpression[i].slice(0, factorialDobleIndex);
-          //alert(number);
           var factorial = 1;
           var count = 0; 
 //for EVEN
@@ -36,19 +32,18 @@ module.exports = function zeros(expression) {
             countEven = 1;
             for (var j = 2; j < number; ) {
               if ((j + 2) % 10 === 0 ) {
-                count = count + 1;//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                //alert(j);
+                count = count + 1;
               }
             factorial = "" + (factorial * (j + 2));
             j = j + 2;
             }
           }
+//for ODD
           else {
             for (var j = 1; j < number; ) {
               if (((j + 2) % 5 === 0) && ((j + 2) % 10 !== 0) && (countEven === 1) && ((j + 2) % 25 === 0)) {
-                count = count + 2;//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                count = count + 2;
                 countEven = 0;
-                //alert(j);
               }
               else if (((j + 2) % 5 === 0) && ((j + 2) % 10 !== 0) && (countEven === 1)) {
                 count = count + 1;
@@ -63,14 +58,4 @@ module.exports = function zeros(expression) {
         zeros = zeros + count;
       }
       return zeros;
-      //alert(result);
-      //alert(zeros);
-      //var i = 0;
-      //do {
-      //  zeros = zeros + 1;
-      //  //alert(zeros);
-      //  i = i + 1;
-      //}
-      //while (result.charAt(result.length - i) == '0');
-      //alert(zeros - 1);
 }
